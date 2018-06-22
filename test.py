a122504c4dfc6bc5a4addbfc16f9d72df7b1c4f7
Tim Harrison <tim@twharr.com>
2018-06-22 13:38:19 -0700

    a set of simple tests for settings & atbat

---
+++ test.py
@@ -0,0 +1,32 @@
+import settings
+
+print('\n\nCreating Starter')
+testStarter = settings.Starter('2B')
+print(testStarter)
+
+print('\n\nCreating Pinch Hitter')
+testPinch = settings.Pinch('2B')
+print(testPinch)
+
+print('\n\nCreating Pitcher')
+testPitcher = settings.Pitcher('SP')
+print(testPitcher)
+
+print('\n\nCreating team a')
+testTeamA = settings.Team('Test Team')
+print(testTeamA.printPlayers())
+
+print('\n\nGenerating lineup a')
+testLineupA = testTeamA.generateLineup(testTeamA.players)
+print(testTeamA.printLineup())
+
+print('\n\nCreating team b')
+testTeamB = settings.Team('Test Team')
+print(testTeamB.printPlayers())
+
+print('\n\nGenerating lineup b')
+testLineupB = testTeamB.generateLineup(testTeamB.players)
+print(testTeamB.printLineup())
+
+settings.startingLineups(testTeamA)
+settings.startingLineups(testTeamB)
